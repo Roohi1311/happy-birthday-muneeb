12791d468992c414ad23bb09b5a731dabaf5072b
Roohi1311 <raviha1311shaukat@gmail.com>
2026-07-11 17:04:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,19 @@ function showSlideshow(){
     },3000);
 
 }
+function createFirework() {
+
+    const firework = document.createElement("div");
+    firework.className = "firework";
+
+    firework.style.left = Math.random() * window.innerWidth + "px";
+    firework.style.top = Math.random() * window.innerHeight + "px";
+
+    document.body.appendChild(firework);
+
+    setTimeout(() => {
+        firework.remove();
+    }, 1000);
+}
+
+setInterval(createFirework, 1500);
