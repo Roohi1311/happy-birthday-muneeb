798dfe948ba8f6c8736e8442ee12a26dba24aa84
Roohi1311 <raviha1311shaukat@gmail.com>
2026-07-11 13:47:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Happy Bithday Muneeb 💕");
+const startBtn = document.getElementById("startBtn");
+const intro = document.getElementById("intro");
+const surprise = document.getElementById("surprise");
+
+startBtn.addEventListener("click", () => {
+    intro.style.display = "none";
+    surprise.style.display = "block";
+
+    startCountdown();
+});
+
+function startCountdown() {
+    let count = 5;
+    const countdown = document.getElementById("countdown");
+
+    countdown.innerHTML = count;
+
+    const timer = setInterval(() => {
+        count--;
+
+        if (count > 0) {
+            countdown.innerHTML = count;
+        } else {
+            clearInterval(timer);
+            countdown.innerHTML = "Surprise ❤️";
+            showMessage();
+        }
+    }, 1000);
+}
+
+function showMessage() {
+    const text = "I have something special for you ✨";
+    const message = document.getElementById("typewriter");
+
+    let i = 0;
+
+    function typing() {
+        if (i < text.length) {
+            message.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typing, 100);
+        }
+    }
+
+    typing();
+}
