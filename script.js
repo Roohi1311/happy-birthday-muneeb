9aaa2b14941809681563d37182a1b4b47d8a607e
Roohi1311 <raviha1311shaukat@gmail.com>
2026-07-11 17:42:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -74,13 +74,13 @@ function showSlideshow(){
 
     document.body.insertAdjacentHTML("beforeend",`
         <div class="slideshow">
-            <img id="slide" src="Mm.jfif">
+            <img id="slide" src="Mmm.jfif">
             <h2>Always Be With,<br>You ❤️</h2>
         </div>
     `);
 
     let images=[
-        "Mm.jfif",
+        "Mmm.jfif",
         "Muneeb.jfif"
     ];
 
